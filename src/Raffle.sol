addadc8dc91eb3294218404d7959e69cf5c65260
Oluwafemi <femio82@gmail.com>
2025-01-21 22:46:06 +0100

    complete:entr raffle function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,14 +9,29 @@ pragma solidity 0.8.19;
  * @dev Implements Chainlink VRFv2.5
  */
 contract Raffle {
+    /**Errors */
+    error Raffle__SendMoreEthToEnterRaffle();
     //i_ stands for immutable, signifying that the varaible cnnot be changed
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /**Events */
+
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreEthToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+        //Emit event each time storage is updated
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
