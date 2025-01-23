599866737bd9caf579d79b125f03d1166d2117ec
Oluwafemi <femio82@gmail.com>
2025-01-23 19:59:31 +0100

    writing tests

---
+++ test/uint/RaffleTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployRaffle} from "script/DeployRaffle.s.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {Raffle} from "src/Raffle.sol";
+
+contract RaffleTest is Test {
+    Raffle public raffle;
+    HelperConfig public helperconfig;
+
+    address public
+
+    function setUp() external {
+        DeployRaffle deployer = new DeployDaffle();
+        (raffle, helperconfig) = deployer.deployContract();
+    }
+}
