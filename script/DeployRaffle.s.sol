36cb8ff6abea77ffc9fed9fb89259f47df7c0a95
Oluwafemi <femio82@gmail.com>
2025-01-24 16:03:48 +0100

    complete deployraffle

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {CreateSubscription} from "script/Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
@@ -18,6 +18,14 @@ contract DeployRaffle is Script {
             CreateSubscription createsubscription = new CreateSubscription();
             (config.subscriptionId, config.vrfCoordinator) = createsubscription
                 .createSubscription(config.vrfCoordinator);
+            //Fund it
+            FundSubscription fundsubscription = new FundSubscription();
+            fundsubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.link
+            );
+            //AddConsuymer
         }
 
         vm.startBroadcast();
@@ -30,6 +38,13 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addconsumer = new AddConsumer();
+        addconsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordinator,
+            config.subscriptionId
+        );
         return (raffle, helperconfig);
     }
 }
